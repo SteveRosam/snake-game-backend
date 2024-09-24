ca002a05b25f14190f2d449d52a1dc9adb3b19ef
Steve Rosam <steve+quix-org@quix.io>
2024-09-24 09:04:26 +0000

    Edited 'main.py'

--- redis test/main.py
+++ redis test/main.py
@@ -1,23 +1,57 @@
-import os
-from quixstreams import Application
+import redis
+import json
 
-# for local dev, load env vars from a .env file
-from dotenv import load_dotenv
-load_dotenv()
+# Connect to Redis
+# client = redis.Redis(host='redis', port=6379, decode_responses=True)
+server_url = '10.244.10.96'
+port = 80 # 6379
 
-app = Application(consumer_group="transformation-v1", auto_offset_reset="earliest")
+def clear():
+    client = redis.Redis(host=server_url, port=port, db=0)
 
-input_topic = app.topic(os.environ["input"])
-output_topic = app.topic(os.environ["output"])
+    client.flushdb()
 
-sdf = app.dataframe(input_topic)
+def test_redis():
+    print("connecting")
+    # Connect to Redis server
+    client = redis.Redis(host=server_url, port=port, db=0)
+    print("Connected")
 
-# put transformation logic here
-# see docs for what you can do
-# https://quix.io/docs/get-started/quixtour/process-threshold.html
+    print("Testing 1")
+    # Test setting a key
+    # client.set('test_key', 'test_value')
 
-sdf.print()
-sdf.to_topic(output_topic)
+    print("Testing 2")
+    # Test getting the key
+    value = client.get('test_key').decode('utf-8')
 
-if __name__ == "__main__":
-    app.run(sdf)
+    print("Testing 3")
+    # Print the result
+    print(f"Value for 'test_key': {value}")
+
+    # Clean up
+    client.delete('test_key')
+
+    print("Done")
+
+def get_all_keys():
+
+    client = redis.Redis(host=server_url, port=port, decode_responses=True)
+
+    # Get all keys from Redis
+    keys = client.keys('*')
+
+    # Print all keys and their values
+    for key in keys:
+        value = client.get(key)
+        try:
+            # Try to parse the value as JSON
+            value = json.loads(value)
+        except json.JSONDecodeError:
+            pass  # If it's not JSON, just keep the value as is
+        print(f"{key}: {value}")
+
+
+# clear()
+test_redis()
+# get_all_keys()
